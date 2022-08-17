73a01cd9bc13565970e203ec768c56ef753353f4
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-17 20:09:50 -0300

    feat: criando o arquivo auth para login no service

---
+++ app/backend/src/services/auth.service.ts
@@ -0,0 +1,12 @@
+import Login, { createLogin } from '../interfaces/ILogin';
+// import User from '../database/models/User';
+import JwtService from './jwt.service';
+
+export default class Auth implements createLogin {
+  static async login(login: Login): Promise<string> {
+    const { email, password } = login;
+    // const user = User.findAll();
+    const token = JwtService.sign({ email, password });
+    return token;
+  }
+}
