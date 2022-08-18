9151a3221877d25a1a7633025c45a897ef0d4f1b
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-18 16:14:17 -0300

    feat: criando a verificação do jwt

--- app/backend/src/services/jwt.service.ts
+++ app/backend/src/services/jwt.service.ts
@@ -1,8 +1,20 @@
-import { sign } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import UserFace from '../interfaces/IUser';
 
 export default class JwtService {
   static sign(payload: { email: string, password: string }): string {
-    return sign(payload, process.env.JWT_SECRET || 'jwt_secret');
+    return jwt.sign(payload, process.env.JWT_SECRET || 'jwt_secret');
+  }
+
+  static verifyToken(token: string) {
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET || 'jwt_secret');
+      return data as UserFace;
+    } catch (e) {
+      const error = new Error('Invalid token');
+      error.name = 'Unauthorized';
+      throw error;
+    }
   }
 }
