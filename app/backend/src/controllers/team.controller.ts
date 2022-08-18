dd200d3057b97cb1a51b3cfa685bfaa8eaa0b6d8
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-18 18:51:42 -0300

    feat: criando o controller de team

---
+++ app/backend/src/controllers/team.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import TeamService from '../services/team.service';
+
+export default class TeamController {
+  constructor(private teamService: TeamService) {}
+
+  async list(req: Request, res: Response): Promise<void> {
+    const result = await this.teamService.list();
+    res.status(200).json(result);
+  }
+
+  async listById(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const result = await this.teamService.listById(Number(id));
+    res.status(200).json(result);
+  }
+}
