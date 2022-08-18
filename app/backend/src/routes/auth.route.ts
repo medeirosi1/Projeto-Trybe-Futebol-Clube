2c553d70cf27fcdc3e6bf0fe2caf28b3eec1ab55
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-18 16:14:47 -0300

    fix: colocando a rota get de validadte

--- app/backend/src/routes/auth.route.ts
+++ app/backend/src/routes/auth.route.ts
@@ -7,5 +7,6 @@ const authController = new AuthController(new Auth());
 const authRouter = Router();
 
 authRouter.post('/', (req, res) => authController.login(req, res));
+authRouter.get('/validate', (req, res) => authController.loginValidate(req, res));
 
 export default authRouter;
