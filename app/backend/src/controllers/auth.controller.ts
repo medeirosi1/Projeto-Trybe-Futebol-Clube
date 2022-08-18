1dde6aac66348a24f3f71cee17729998468a6acd
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-18 16:16:23 -0300

    feat: colocando a validção no controller

--- app/backend/src/controllers/auth.controller.ts
+++ app/backend/src/controllers/auth.controller.ts
@@ -5,7 +5,15 @@ export default class AuthController {
   constructor(private authService: Auth) {}
 
   async login(req: Request, res: Response): Promise<void> {
-    const token = await this.authService.login(req.body);
+    const validate = this.authService.validateLogin(req.body);
+    const token = await this.authService.login(validate);
     res.status(200).json({ token });
+  }
+
+  async loginValidate(req: Request, res: Response):Promise<void> {
+    const token = req.headers.authorization;
+    const tokenToString = String(token);
+    const role = await this.authService.loginValidate(tokenToString);
+    res.status(200).json({ role });
   }
 }
