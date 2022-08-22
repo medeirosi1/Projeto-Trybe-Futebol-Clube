28356696bfbcd4db80fd559c80db0daad0d04ce0
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 12:06:04 -0300

    fix: adicionando mais interaces para match

--- app/backend/src/interfaces/IMatch.ts
+++ app/backend/src/interfaces/IMatch.ts
@@ -6,3 +6,33 @@ export default interface MatchFace {
   awayTeamGoals: number,
   inProgress: boolean,
 }
+
+export interface MatchFull {
+  id?: number,
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+  teamHome: {
+    teamName: string
+  },
+  teamAway: {
+    teamName: string
+  }
+}
+
+export interface TeamMatchHome {
+  id: number,
+  teamName: string,
+  homeMatch: [
+    {
+      id: number,
+      homeTeam: number,
+      homeTeamGoals: number,
+      awayTeam: number,
+      awayTeamGoals: number,
+      inProgress: boolean
+    },
+  ]
+}
