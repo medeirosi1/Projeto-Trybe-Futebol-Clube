ee03427657132ff4d322b9672a756db5dfad4fab
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 12:05:44 -0300

    feat: criando o mock para match

---
+++ app/backend/src/tests/mock/matchMock.ts
@@ -0,0 +1,34 @@
+import Match from "../../database/models/Match"
+
+const listMock = [
+    {
+      "id": 1,
+      "homeTeam": 16,
+      "homeTeamGoals": 1,
+      "awayTeam": 8,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+    //   "teamHome": {
+    //     "teamName": "São Paulo"
+    //   },
+    //   "teamAway": {
+    //     "teamName": "Grêmio"
+    //   }
+    },
+    {
+      "id": 2,
+      "homeTeam": 9,
+      "homeTeamGoals": 1,
+      "awayTeam": 14,
+      "awayTeamGoals": 1,
+      "inProgress": false,
+    //   "teamHome": {
+    //     "teamName": "Internacional"
+    //   },
+    //   "teamAway": {
+    //     "teamName": "Santos"
+    //   }
+    }
+  ] as Match[]
+
+export default listMock
