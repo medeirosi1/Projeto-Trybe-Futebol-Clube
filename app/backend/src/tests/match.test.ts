8f001a40169af7d99eabf016ec83f5411dec7301
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-23 10:01:02 -0300

    fix: arrumando o teste de match

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -6,7 +6,8 @@ import chaiHttp = require('chai-http');
 import { app } from '../app'
 // import { Response } from 'superagent';
 import Match from '../database/models/Match';
-import listMock from './mock/matchMock';
+import listMock, { matchAll } from './mock/matchMock';
+import { MatchFull } from '../interfaces/IMatch';
 
 chai.use(chaiHttp);
 
@@ -53,23 +54,29 @@ describe('Testando a rota /matches/:id/finish', () => {
       expect(match.body.message).to.deep.eq('Finished');
     });
   })
-  
 
-// describe('Testando a rota /matches/:id/finish', () => {
-
-//     before(async () => {
-//       sinon
-//         .stub(Match, 'findAll')
-//         .resolves(listMock as Match[])
-//       })
+  describe('Testando a rota /matches/?InProgress', () => {
+    
+    before(async () => {
+      sinon
+        .stub(Match, 'findAll')
+        .resolves(matchAll as unknown as Match[])
+      })
   
-//     after(()=>{
-//       sinon.restore();
-//     })
+    after(()=>{
+      sinon.restore();
+    })
   
-//     it('Espera que o retorno seja 200 e retorna uma mensagem top', async () => {
-//       const match = await chai.request(app).patch('/matches/:id/finish')
-//       expect(match.status).to.be.eq(200);
-//       expect(match.body.message).to.deep.eq('Finished');
-//     });
-// })
+    it('Espera que o retorno seja 200', async () => {
+      const match = await chai.request(app).get('/matches/?InProgress=false')
+      expect(match.status).to.be.eq(200);
+    });
+    it('Espera que o retorno do body seja quando colocar false', async () => {
+      const match = await chai.request(app).get('/matches/?InProgress=false')
+      expect(match.body).to.deep.eq(matchAll);
+    });
+    it('Espera que o retorno do body seja quando colocar false', async () => {
+      const match = await chai.request(app).get('/matches/?InProgress=true')
+      expect(match.body).to.deep.eq(matchAll);
+    });
+  });
