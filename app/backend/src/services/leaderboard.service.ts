d84d9df31a52b2bfd79fd4b77dd3f9a4bc6c7d78
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 23:53:50 -0300

    fix: arrumando exportações e separando funções

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -14,7 +14,7 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
 import { TeamMatchHome } from '../interfaces/IMatch';
-import homeTeam from '../interfaces/ILeaderboard';
+import homeLeaderboard from './utils';
 
 export default class LeaderboardService {
   listHome = (points: number[]) => points;
@@ -26,19 +26,19 @@ export default class LeaderboardService {
     return matchall as unknown as TeamMatchHome[];
   };
 
-  async leaderboardTeam() {
+  async leaderboardHomeTeam() {
     const teamHome = await this.homeTeams();
     const leaderboard = teamHome.map((home) => ({
       name: home.teamName,
-      totalPoints: this.totalPoints(home.homeMatch),
-      totalGames: this.totalGames(home.homeMatch),
-      totalVictories: this.totalVictories(home.homeMatch),
-      totalDraws: this.totalDraws(home.homeMatch),
-      totalLosses: this.totalLosses(home.homeMatch),
-      goalsFavor: this.goalsFavor(home.homeMatch),
-      goalsOwn: this.goalsOwn(home.homeMatch),
-      goalsBalance: this.goalsBalance(home.homeMatch),
-      efficiency: this.efficiency(home.homeMatch),
+      totalPoints: homeLeaderboard.totalPoints(home.homeMatch),
+      totalGames: homeLeaderboard.totalGames(home.homeMatch),
+      totalVictories: homeLeaderboard.totalVictories(home.homeMatch),
+      totalDraws: homeLeaderboard.totalDraws(home.homeMatch),
+      totalLosses: homeLeaderboard.totalLosses(home.homeMatch),
+      goalsFavor: homeLeaderboard.goalsFavor(home.homeMatch),
+      goalsOwn: homeLeaderboard.goalsOwn(home.homeMatch),
+      goalsBalance: homeLeaderboard.goalsBalance(home.homeMatch),
+      efficiency: homeLeaderboard.efficiency(home.homeMatch),
     }));
     return leaderboard.sort((acc, curr) => curr.totalPoints - acc.totalPoints
     || curr.totalVictories - acc.totalVictories
@@ -46,72 +46,4 @@ export default class LeaderboardService {
     || curr.goalsFavor - acc.goalsFavor
     || curr.goalsOwn - acc.goalsOwn);
   }
-
-  totalGames = (home: homeTeam[]) => home.length;
-
-  totalPoints = (home: homeTeam[]) => {
-    const totalPoints = this.totalVictories(home) * 3 + this.totalDraws(home);
-    return totalPoints;
-  };
-
-  totalVictories = (home: homeTeam[]) => {
-    const vitorias = home.map((el) => {
-      let count = 0;
-      if (el.homeTeamGoals > el.awayTeamGoals) {
-        count += 1;
-      }
-      return count;
-    });
-    return vitorias.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  totalDraws = (home: homeTeam[]) => {
-    const draws = home.map((el) => {
-      let count = 0;
-      if (el.homeTeamGoals === el.awayTeamGoals) {
-        count += 1;
-      }
-      return count;
-    });
-    return draws.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  totalLosses = (home: homeTeam[]) => {
-    const losses = home.map((el) => {
-      let count = 0;
-      if (el.homeTeamGoals < el.awayTeamGoals) {
-        count += 1;
-      }
-      return count;
-    });
-    return losses.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  goalsFavor = (home: homeTeam[]) => {
-    const goals = home.map((el) => {
-      let count = 0;
-      count += el.homeTeamGoals;
-      return count;
-    });
-    return goals.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  goalsOwn = (home: homeTeam[]) => {
-    const goals = home.map((el) => {
-      let count = 0;
-      count += el.awayTeamGoals;
-      return count;
-    });
-    return goals.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  goalsBalance = (home: homeTeam[]) => {
-    const balance = this.goalsFavor(home) - this.goalsOwn(home);
-    return balance;
-  };
-
-  efficiency = (home: homeTeam[]) => {
-    const efficiency = (this.totalPoints(home) / (this.totalGames(home) * 3)) * 100;
-    return efficiency.toFixed(2);
-  };
 }
