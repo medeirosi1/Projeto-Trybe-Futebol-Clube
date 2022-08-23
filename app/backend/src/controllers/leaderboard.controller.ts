bf339776aa9a66810bd6e9aceba1b93931d94a98
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 23:54:23 -0300

    feat: adicioando o controller de listaway

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from 'express';
+import AwayLeaderboardService from '../services/awayLeaderboard.service';
 import LeaderboardService from '../services/leaderboard.service';
 
 export default class LeaderboardController {
   constructor(
     private leaderboardService: LeaderboardService,
+    private awayLeaderboardService: AwayLeaderboardService,
   ) {}
 
   async listHome(_req: Request, res: Response): Promise<void> {
     // const point = await this.leaderboardService.totalPoints();
-    const leaderboardHome = await this.leaderboardService.leaderboardTeam();
+    const leaderboardHome = await this.leaderboardService.leaderboardHomeTeam();
     // const result = await this.leaderboardService.listHome(point);
     res.status(200).json(leaderboardHome);
+  }
+
+  async listaway(_req: Request, res: Response): Promise<void> {
+    const leaderboardAway = await this.awayLeaderboardService.leaderboardTeamAway();
+    res.status(200).json(leaderboardAway);
   }
 }
