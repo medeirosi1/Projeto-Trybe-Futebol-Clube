490ff6694da310795e04621528a961321c9c7106
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-23 09:10:07 -0300

    feat: criando a rota para pegar todos os placares

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -2,16 +2,18 @@ import { Router } from 'express';
 import AwayLeaderboardService from '../services/awayLeaderboard.service';
 import LeaderboardController from '../controllers/leaderboard.controller';
 import LeaderboardService from '../services/leaderboard.service';
+import AllLeaderboardService from '../services/allLeaderboard.service';
 
 const leaderboardController = new LeaderboardController(
   new LeaderboardService(),
   new AwayLeaderboardService(),
+  new AllLeaderboardService(),
 );
 
 const leaderboardRouter = Router();
 
 leaderboardRouter.get('/home', (req, res) => leaderboardController.listHome(req, res));
 leaderboardRouter.get('/away', (req, res) => leaderboardController.listaway(req, res));
-// leaderboardRouter.patch('/:id', (req, res) => matchController.matchUpdated(req, res));
+leaderboardRouter.get('/', (req, res) => leaderboardController.listAll(req, res));
 
 export default leaderboardRouter;
