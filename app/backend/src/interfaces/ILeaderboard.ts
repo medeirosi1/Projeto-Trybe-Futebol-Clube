9ca86a40c3d54ecb2df227523c928b07a329f6f2
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 23:52:41 -0300

    feat: adicioando mais interfaces leaderboards

--- app/backend/src/interfaces/ILeaderboard.ts
+++ app/backend/src/interfaces/ILeaderboard.ts
@@ -6,3 +6,16 @@ export default interface homeTeam {
   awayTeamGoals: number,
   inProgress: boolean
 }
+
+export interface leaderboardTeam {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories:number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: string,
+}
