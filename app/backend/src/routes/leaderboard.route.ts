aaa843990a6dd34e91c53600e02f926f9e34b36d
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-22 23:52:56 -0300

    feat: adicionando a rota away no get

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
+import AwayLeaderboardService from '../services/awayLeaderboard.service';
 import LeaderboardController from '../controllers/leaderboard.controller';
 import LeaderboardService from '../services/leaderboard.service';
 
 const leaderboardController = new LeaderboardController(
   new LeaderboardService(),
+  new AwayLeaderboardService(),
 );
 
 const leaderboardRouter = Router();
 
 leaderboardRouter.get('/home', (req, res) => leaderboardController.listHome(req, res));
+leaderboardRouter.get('/away', (req, res) => leaderboardController.listaway(req, res));
 // leaderboardRouter.patch('/:id', (req, res) => matchController.matchUpdated(req, res));
 
 export default leaderboardRouter;
